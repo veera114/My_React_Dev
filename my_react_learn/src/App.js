2cbcae3c8105ebe06e0b48a1373882415ebb720b
Ashok Mallagunta <a.mallagunta@gmail.com>
2024-07-13 01:43:37 +0530

    test

---
+++ my_react_learn/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Example1 from './Components/BasicHello'
+
+const App = () => {
+  return (
+    <div>
+      <Example1 />
+    </div>
+  )
+}
+
+export default App
---
+++ my_react_learn/src/Components/BasicHello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Example1 = () => {
+  return (
+    <div>
+        <h1>Hello world of React!</h1>
+    </div>
+  )
+}
+
+export default Example1
